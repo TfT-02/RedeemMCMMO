c3301993e52a43318fd74470ccbe866d565bfd96
riking <rikingcoding@gmail.com>
2013-05-26 12:50:57 -0700

    Reformat listener

--- src/main/java/com/flapfactions/redeemMCMMO/playerListener.java
+++ src/main/java/com/flapfactions/redeemMCMMO/playerListener.java
@@ -11,39 +11,39 @@ import org.bukkit.event.player.PlayerLoginEvent;
 
 public class playerListener implements Listener {
 
-	public RedeemMCMMO plugin;
+    public RedeemMCMMO plugin;
 
-	public playerListener(RedeemMCMMO plugin) {
-	    this.plugin = plugin;
-	}
+    public playerListener(RedeemMCMMO plugin) {
+        this.plugin = plugin;
+    }
 
-	@EventHandler
-	public void onLogin(PlayerLoginEvent event) {
-		Player player = event.getPlayer();
-		File playerDat = new File("world/players/"+ player.getName() + ".dat");
-		if(!playerDat.exists()) {
-			String playerName = player.getName();
-			int startupAmount = plugin.getConfig().getInt("startupAmount");
-			if(startupAmount == 0) {
-				//do nothing if startupAmount is 0
-			} else {
-				plugin.getConfig().set(playerName + ".credits", startupAmount);
-				plugin.saveConfig();
-			}
-		}
-	}
+    @EventHandler
+    public void onLogin(PlayerLoginEvent event) {
+        Player player = event.getPlayer();
+        File playerDat = new File("world/players/" + player.getName() + ".dat");
+        if (!playerDat.exists()) {
+            String playerName = player.getName();
+            int startupAmount = plugin.getConfig().getInt("startupAmount");
+            if (startupAmount == 0) {
+                //do nothing if startupAmount is 0
+            } else {
+                plugin.getConfig().set(playerName + ".credits", startupAmount);
+                plugin.saveConfig();
+            }
+        }
+    }
 
-	@EventHandler
-	public void onJoin(PlayerJoinEvent event) {
-		Player player = event.getPlayer();
-		int credits = plugin.getConfig().getInt(player.getName() + ".credits");
-		if(credits == 0) {
-			//do nothing!
-		} else {
-			if(plugin.getConfig().getBoolean("joinMessage") == true) {
-			player.sendMessage(ChatColor.GREEN + "You have " + ChatColor.GOLD + credits + ChatColor.GREEN + " MCMMO credits.");
-			player.sendMessage(ChatColor.GREEN + "Use " + ChatColor.GOLD + "/redeem <skill> <amount>" + ChatColor.GREEN + " to redeem them.");
-			}
-		}
-	}
+    @EventHandler
+    public void onJoin(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+        int credits = plugin.getConfig().getInt(player.getName() + ".credits");
+        if (credits == 0) {
+            //do nothing!
+        } else {
+            if (plugin.getConfig().getBoolean("joinMessage") == true) {
+                player.sendMessage(ChatColor.GREEN + "You have " + ChatColor.GOLD + credits + ChatColor.GREEN + " MCMMO credits.");
+                player.sendMessage(ChatColor.GREEN + "Use " + ChatColor.GOLD + "/redeem <skill> <amount>" + ChatColor.GREEN + " to redeem them.");
+            }
+        }
+    }
 }
